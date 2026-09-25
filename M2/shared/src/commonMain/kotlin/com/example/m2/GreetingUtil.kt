8b3d59a1f86c8e3956cd20a3ab039bae80236964
Avardhra <gedevalendra738@gmail.com>
2026-09-25 12:37:46 +0700

    update-otomatis

---
+++ M2/shared/src/commonMain/kotlin/com/example/m2/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.m2
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ M2/shared/src/iosMain/kotlin/com/example/m2/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.m2
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
